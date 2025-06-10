be625b9ecdebe84a0c16b3f58f901a9b6aaa1ba7
Leonardo Muniz <munizzleonardo@gmail.com>
2025-06-10 17:05:18 -0300

    Commit
    Alterações e criação de uma nova página.

--- js/unienfjs.js
+++ js/unienfjs.js
@@ -9,37 +9,3 @@
       header.classList.remove("sticky");
     }
   }
-
-
-// Seleciona o contêiner de cursos
-  const cursoContainer = document.querySelector('.curso-container');
-    
-// Função para mover os cursos para a esquerda
-  function moveCursos() {
-// Move a "cursos-container" para a esquerda, fazendo com que o primeiro curso desapareça
-  cursoContainer.style.transition = 'transform 1s ease-in-out'; // Define o tempo da transição
-  cursoContainer.style.transform = 'translateX(-33%)'; // Desloca para a esquerda em 33% da largura
-}
-  
-// Função para resetar a posição para o começo
-  function resetCursos() {
-// Reseta a posição de volta ao início
-
-  setTimeout(() => {
-    cursoContainer.style.transition = 'none'; // Retira a transição temporariamente
-    cursoContainer.style.transform = 'translateX(0)';
-        
-// Após resetar, adiciona a transição novamente
-  setTimeout(() => {
-  cursoContainer.style.transition = 'transform 1s ease-in-out';
-  }, 50);
-
-  }, 1000); // Espera o tempo da animação antes de resetar
-          
-}
-  
-// Configura o intervalo de movimento dos cursos
-  setInterval(() => {
-    moveCursos();
-    resetCursos();
-  }, 4000); // A cada 4 segundos a transição acontece
